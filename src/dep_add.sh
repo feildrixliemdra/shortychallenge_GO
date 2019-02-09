69e5493890ab39941cc0ac9be232ac0d6c8a30c6
Andi Ashari <mq.aashari@gmail.com>
2019-02-09 19:10:08 +0700

    Update dep_add

--- src/dep_add.sh
+++ src/dep_add.sh
@@ -5,3 +5,5 @@ rm -rf /go/src/github.com/ralali/rl-ms-boilerplate-go;
 ln -s /my_app /go/src/github.com/ralali/rl-ms-boilerplate-go;
 cd /go/src/github.com/ralali/rl-ms-boilerplate-go;
 dep ensure -add $1;
+go get $1;
+rm -rf vendor
